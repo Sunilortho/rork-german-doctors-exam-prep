d4d039f65505dc2dd0ef3450ac646714d310bd31
Sunilortho <Drsunilortho0007@gmail.com>
2026-01-12 21:01:36 +0100

    Update babel config to properly handle module aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          unstable_transformImportMeta: true,
+          native: {
+            android: { useTransformReactJsxExperimental: true },
+            ios: { useTransformReactJsxExperimental: true },
+          },
+        },
+      ],
+    ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': './',
+          },
+        },
+      ],
+    ],
   };
 };
